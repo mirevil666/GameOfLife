82ea42577aa33372342b5a2785ca61f31318e7dc
Carlos Hernández Pérez <carlos.hernandez@tralix.com>
2014-11-06 14:01:10 -0600

    dia 2

--- src/main/scala/com/game/GameOfLife.scala
+++ src/main/scala/com/game/GameOfLife.scala
@@ -1,12 +1,25 @@
 package com.game
 
-object GameOfLife {
-  def setCelulaPosition(ubucacion: Ubicacion) = {
+class GameOfLife {
+
+  val world = Array.ofDim[Celula](3, 3)
+
+  def nextStepTime = {
+    println(world(1)(0))
+    println(world(0)(1))
+    if (world(0)(1) == null || world(1)(0) == null) {
+      println("entro al if")
+      world(0)(0) = null
+    }
 
   }
 
+  def setCelula(celula: Celula) = {
+    world(celula.ubucacion.x)(celula.ubucacion.y) = celula
+  }
+
   def getCelula(ubucacion: Ubicacion): Celula = {
-    Celula(Ubicacion(1, 1))
+    world(ubucacion.x)(ubucacion.y)
   }
 
 }
--- src/test/scala/com/game/GameOfLifeTest.scala
+++ src/test/scala/com/game/GameOfLifeTest.scala
@@ -4,13 +4,29 @@ import org.specs2.mutable._
 
 class GameOfLifeTest extends Specification {
 
-  "Si la celula no tiene vecino" should {
-    "morir" in {
-      val game = GameOfLife
+  "Any live cell with fewer than two live neighbors" should {
+    "die" in {
+      val game = new GameOfLife
       val ubicacion = Ubicacion(0, 0)
-      game.setCelulaPosition(ubicacion)
+      game.setCelula(Celula(ubicacion))
+      game.nextStepTime
       game.getCelula(ubicacion) must_== (null)
     }
+    "Live cell in (0,0) with two live neighbors (0,1) (1,0)" should {
+      " lives" in{
+        val game = new GameOfLife
+        val neighbor1 = Celula(Ubicacion(0, 1))
+        val neighbor2 = Celula(Ubicacion(1, 0))
+        val celulaAevaluar = Celula(Ubicacion(0, 0))
+        game.setCelula(celulaAevaluar)
+        game.setCelula(neighbor1)
+        game.setCelula(neighbor2)
+        game.nextStepTime
+        game.getCelula(Ubicacion(0, 0)) must beEqualTo(celulaAevaluar)
+      }
+    }
+
+
   }
 
 }
